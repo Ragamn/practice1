ec718abe36a79c7b886d6a4adffe030148c4ca83
riki <ri.takahashi.sys22@morijyobi.ac.jp>
2022-09-27 10:25:49 +0900

    pra2.javaを編集しました。 クラスを追記しました。

--- pra2.java
+++ pra2.java
@@ -1,5 +1,7 @@
-public static void main(String args){
-	for(int i = 0;i<100;i++){
-	system.out.println("Hello World");
-	}
+public class pra2.java{	
+	public static void main(String args){
+		for(int i = 0;i<100;i++){
+		system.out.println("Hello World");
+		}
+	}	
 }
